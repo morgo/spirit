3063f106f617ab33cfc44410271ab7f2512d7807
Kolbe Kegel <kolbek@squareup.com>
2025-10-20 20:30:54 -0700

    Make invisible_index_before_drop configurable and modify RunLinters to return an error

--- pkg/lint/lint_invisible_index.go
+++ pkg/lint/lint_invisible_index.go
@@ -1,7 +1,9 @@
 package lint
 
 import (
+	"errors"
 	"fmt"
+	"strings"
 
 	"github.com/block/spirit/pkg/statement"
 	"github.com/pingcap/tidb/pkg/parser/ast"
@@ -13,7 +15,9 @@ func init() {
 
 // InvisibleIndexBeforeDropLinter checks that indexes are made invisible before dropping.
 // This is a safety practice to ensure the index is not needed before permanently removing it.
-type InvisibleIndexBeforeDropLinter struct{}
+type InvisibleIndexBeforeDropLinter struct {
+	raiseError bool
+}
 
 func (l *InvisibleIndexBeforeDropLinter) String() string {
 	return Stringer(l)
@@ -27,7 +31,42 @@ func (l *InvisibleIndexBeforeDropLinter) Description() string {
 	return "Requires indexes to be made invisible before dropping them as a safety measure"
 }
 
+func (l *InvisibleIndexBeforeDropLinter) Configure(a any) error {
+	c, ok := a.(map[string]string)
+	if !ok {
+		return errors.New(l.Name() + " config must be a map[string]string")
+	}
+	for k, v := range c {
+		switch k {
+		case "raiseError":
+			if strings.EqualFold(v, "true") {
+				l.raiseError = true
+				break
+			}
+			if strings.EqualFold(v, "false") {
+				l.raiseError = false
+				break
+			}
+			return fmt.Errorf("invalid value for %s: %s", k, v)
+		default:
+			return fmt.Errorf("unknown config key for %s: %s", l.Name(), k)
+		}
+	}
+	return nil
+}
+func (l *InvisibleIndexBeforeDropLinter) DefaultConfig() any {
+	return map[string]string{
+		"raiseError": "true",
+	}
+}
+
+var _ ConfigurableLinter = &InvisibleIndexBeforeDropLinter{}
+
 func (l *InvisibleIndexBeforeDropLinter) Lint(createTables []*statement.CreateTable, statements []*statement.AbstractStatement) []Violation {
+	severity := SeverityWarning
+	if l.raiseError {
+		severity = SeverityError
+	}
 	var violations []Violation
 
 	for _, stmt := range statements {
@@ -70,7 +109,7 @@ func (l *InvisibleIndexBeforeDropLinter) Lint(createTables []*statement.CreateTa
 				suggestion := fmt.Sprintf("First make the index invisible: ALTER TABLE %s ALTER INDEX %s INVISIBLE", tableName, indexName)
 				violations = append(violations, Violation{
 					Linter:   l,
-					Severity: SeverityWarning,
+					Severity: severity,
 					Message:  fmt.Sprintf("Index '%s' should be made invisible before dropping to ensure it's not needed", indexName),
 					Location: &Location{
 						Table: tableName,
